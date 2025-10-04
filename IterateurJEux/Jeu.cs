00f970f583c89784f926caf3b8477b444c2a1ddf
zacharienoletGit <zacharie.nolet007@gmail.com>
2025-10-04 18:17:30 -0400

    Hello

---
+++ IterateurJEux/Jeu.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppplicationIterateurJeux
+{
+    public class Jeu
+    {
+        public string Nom { get; set; }
+        public int Cote { get; set; }
+    }
+
+    public interface IJeuCoteIterator
+    {
+        Jeu JeuCourant { get; }
+        bool MoveNext();
+    }
+
+    public class JeuCoteIterator : IJeuCoteIterator
+    {
+        private JeuCollection _copy; // on veut une copie pour pas altérer la collection initiale
+        private int _index;
+
+        public JeuCoteIterator(JeuCollection collection)
+        {
+            _copy = new JeuCollection();
+            _copy.AddRange(collection);             // copie des éléments
+            _copy.Sort((j2, j1) => j1.Cote.CompareTo(j2.Cote)); // tri par cote (j2 à j1 pour avoir du meilleur au moin bon)
+            _index = -1; // démarre avant le premier élément
+        }
+
+        public Jeu JeuCourant
+        {
+            get
+            {
+                if (_index >= 0 && _index < _copy.Count)
+                {
+                    return _copy[_index];
+                }
+                else
+                {
+                    return null; // peux lever une exception si tu préfères
+                }
+            }
+        }
+
+        public bool MoveNext()
+        {
+            if (_copy.Count > _index + 1)
+            {
+                _index++;
+                return true;
+            }
+            return false; // S'il  y a juste une personne cca rernvoie false
+
+        }
+
+    }
+
+    public class JeuCollection : List<Jeu>
+    {
+        public IJeuCoteIterator GetIterator()
+        {
+            return new JeuCoteIterator(this);
+        }
+    }
+}
---
+++ IterateurJEux/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace AppplicationIterateurJeux
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            JeuCollection jeux = new JeuCollection
+            {
+                new Jeu { Nom = "RS6", Cote = 8 },
+                new Jeu { Nom = "Skyrim", Cote = 9 },
+                new Jeu { Nom = "PubG", Cote = 7 },
+                new Jeu { Nom = "Roblox", Cote = -2 }
+            };
+
+            IJeuCoteIterator iterateurCote = jeux.GetIterator();
+
+            while (iterateurCote.MoveNext())
+            {
+                Console.WriteLine($"{iterateurCote.JeuCourant.Nom} - Cote: {iterateurCote.JeuCourant.Nom}");
+            }
+        }
+    }
+}
